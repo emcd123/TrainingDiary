9e864672358d8d8667b4bbb5719e9462b73586d4
Owen McDonnell <Owen McDonnell@DESKTOP-4LTL6CN>
2020-03-28 13:10:34 +0000

    Created Add Session modal page with demo form.

--- TrainingDiary/TrainingDiary/ClientApp/src/components/Home.js
+++ TrainingDiary/TrainingDiary/ClientApp/src/components/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import authService from './api-authorization/AuthorizeService'
 import './home.css';
-
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 class ExcerciseEntry extends React.Component {
     render() {
         return (
@@ -79,7 +80,7 @@ export class Home extends Component {
                         <h1 id="tabelLabel" >Sessions List</h1>
                     </div>
                     <div class="col col-lg-2">
-                        <button class="btn-lg btn-primary">Add Session</button>
+                        <AddSessionModal buttonLabel="Add Session"/>
                     </div>
                 </div>
                 {contents}
@@ -95,5 +96,55 @@ export class Home extends Component {
         this.setState({ sessions: data, loading: false });
     }
 }
+
+class AddSessionModal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false
+        };
+
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Button color="primary" size="lg" onClick={this.toggle}>{this.props.buttonLabel}</Button>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle}>{new Intl.DateTimeFormat('en-GB', {
+                        month: 'long',
+                        day: '2-digit',
+                        year: 'numeric',
+                    }).format(new Date())}</ModalHeader>
+                    <ModalBody>
+                        <Form>
+                            <FormGroup>
+                                <Label for="exampleEmail">Email</Label>
+                                <Input type="text" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="examplePassword">Password</Label>
+                                <Input type="text" name="password" id="examplePassword" placeholder="password placeholder" />
+                            </FormGroup>                            
+                            <Button>Submit</Button>
+                        </Form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.toggle}>Add</Button>{' '}
+                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        );
+    }
+}
+
 
 
